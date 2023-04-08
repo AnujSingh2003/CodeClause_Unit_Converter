10f27a3f290fc766d8eeff73cbcdf2e8bb0db75b
Anuj <anujsinghdgp123@gmail.com>
2023-04-08 16:06:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var feet =document.getElementById('feet');
+var inch =document.getElementById('inch');
+feet.addEventListener('input',function(){
+    let f = this.value;
+    let i = f*1000;
+    
+    inch.value=i;
+
+});
+inch.addEventListener('input',function(){
+    let i = this.value;
+    let f = i/1000;
+    if(!Number.isInteger(f)){
+        f = f.toFixed(2);
+
+    }
+    feet.value=f;
+
+});
+
